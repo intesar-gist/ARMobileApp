8fe5881af760dbaec7f216c58c2af1dc7fa7f426
Intesar Haider <intesar.haider1@gmail.com>
2017-04-19 16:47:31 +0200

    fix

--- app/src/main/assets/js/data.js
+++ app/src/main/assets/js/data.js
@@ -1,24 +1,6 @@
 
 var pointersData = [
     {
-        "id": 1,
-        "longitude": 9.686970,
-        "latitude": 50.563331,
-        "altitude": 294,
-        "name": "H2",
-        "description": "Bus Stop towards Hochschule. Bus # 06 comes here every 30 minutes towards the direction of Hochschule.",
-        "buildingName": "Bus Stop > HS",
-        "thumbnailURL": "http://i.imgur.com/DnDoIjl.jpg",
-        "buildingImages": [
-            {
-                "src": "https://farm2.staticflickr.com/1043/5186867718_06b2e9e551_b.jpg",
-                "w": 964,
-                "h": 1024,
-                "title": 'Image1 Caption'
-            }
-        ]
-    },
-    {
         "id": 2,
         "longitude": 9.685557,
         "latitude": 50.562698,
@@ -26,15 +8,17 @@ var pointersData = [
         "name": "H1",
         "description": "Bus Stop towards ZOB. Bus # 06 comes here every 30 minutes towards the direction of ZOB / Fulda Bahnhof",
         "buildingName": "BusStop > ZOB",
-        "thumbnailURL": "http://i.imgur.com/DnDoIjl.jpg",
-        "buildingImages": [
-              {
-                  "src": "https://farm2.staticflickr.com/1043/5186867718_06b2e9e551_b.jpg",
-                  "w": 964,
-                  "h": 1024,
-                  "title": 'Image1 Caption'
-              }
-        ]
+        "thumbnailURL": "assets/gallery/hs_fulda.jpg"
+    },
+    {
+        "id": 1,
+        "longitude": 9.686970,
+        "latitude": 50.563331,
+        "altitude": 294,
+        "name": "H2",
+        "description": "Bus Stop towards Hochschule. Bus # 06 comes here every 30 minutes towards the direction of Hochschule.",
+        "buildingName": "Bus Stop > HS",
+        "thumbnailURL": "assets/gallery/hs_fulda.jpg"
     },
     {
         "id": 3,
@@ -44,40 +28,40 @@ var pointersData = [
         "name": "C (43)",
         "description": "Offices of the concerned persons for international student's consultation specially M.Sc. GSD group. Advisers such as Ms. Hirth, Mrs. Beate etc. sits here. Also issues related to System2Teach could be solved by visiting Room # 07.",
         "buildingName": "Building # 43 (C)",
-        "thumbnailURL": "http://i.imgur.com/DnDoIjl.jpg",
+        "thumbnailURL": "assets/gallery/C/1.jpg",
         "buildingImages": [
             {
-                "src": "https://s3.eu-central-1.amazonaws.com/ar-fulda-app/C/c+(1).jpg",
+                "src": "assets/gallery/C/1.jpg",
                 "w": 964,
                 "h": 1024,
                 "title": 'Building # 43 (C)'
             },
             {
-                "src": "https://s3.eu-central-1.amazonaws.com/ar-fulda-app/C/c+(2).jpg",
+                "src": "assets/gallery/C/2.jpg",
                 "w": 964,
                 "h": 1024,
                 "title": 'Building # 43 (C)'
             },
             {
-                "src": "https://s3.eu-central-1.amazonaws.com/ar-fulda-app/C/c+(3).jpg",
+                "src": "assets/gallery/C/3.jpg",
                 "w": 964,
                 "h": 1024,
                 "title": 'Building # 43 (C)'
             },
             {
-                "src": "https://s3.eu-central-1.amazonaws.com/ar-fulda-app/C/c+(4).jpg",
+                "src": "assets/gallery/C/4.jpg",
                 "w": 964,
                 "h": 1024,
                 "title": 'Building # 43 (C)'
             },
             {
-                "src": "https://s3.eu-central-1.amazonaws.com/ar-fulda-app/C/c+(5).jpg",
+                "src": "assets/gallery/C/5.jpg",
                 "w": 964,
                 "h": 1024,
                 "title": 'Building # 43 (C)'
             },
             {
-                "src": "https://s3.eu-central-1.amazonaws.com/ar-fulda-app/C/c+(6).jpg",
+                "src": "assets/gallery/C/6.jpg",
                 "w": 964,
                 "h": 1024,
                 "title": 'Building # 43 (C)'
@@ -92,88 +76,88 @@ var pointersData = [
         "name": "E (46)",
         "buildingName": "Building # 46 (E)",
         "description": "Main building for Department of Computer Science. All labs such as Linux, Cloud Computing or IOS are here. Major CS classes also held in this same building.",
-        "thumbnailURL": "http://i.imgur.com/DnDoIjl.jpg",
+        "thumbnailURL": "assets/gallery/E/4.jpg",
         "buildingImages": [
             {
-                "src": "https://s3.eu-central-1.amazonaws.com/ar-fulda-app/E/e+(1).jpg",
+                "src": "assets/gallery/E/1.jpg",
                 "w": 1080,
                 "h": 1080,
                 "title": 'Building # 46 (E)'
             },
             {
-                "src": 'https://s3.eu-central-1.amazonaws.com/ar-fulda-app/E/e+(2).jpg',
+                "src": "assets/gallery/E/2.jpg",
                 "w": 1080,
                 "h": 1080,
                 "title": 'Building # 46 (E)'
             },
             {
-                "src": 'https://s3.eu-central-1.amazonaws.com/ar-fulda-app/E/e+(3).jpg',
+                "src": "assets/gallery/E/3.jpg",
                 "w": 1080,
                 "h": 1080,
                 "title": 'Building # 46 (E)'
             },
             {
-                "src": 'https://s3.eu-central-1.amazonaws.com/ar-fulda-app/E/e+(4).jpg',
+                "src": "assets/gallery/E/4.jpg",
                 "w": 1024,
                 "h": 683,
                 "title": 'Building # 46 (E)'
             },
             {
-                "src": 'https://s3.eu-central-1.amazonaws.com/ar-fulda-app/E/e+(5).jpg',
+                "src": "assets/gallery/E/5.jpg",
                 "w": 1024,
                 "h": 683,
                 "title": 'Building # 46 (E)'
             },
             {
-                "src": 'https://s3.eu-central-1.amazonaws.com/ar-fulda-app/E/e+(6).jpg',
+                "src": "assets/gallery/E/6.jpg",
                 "w": 1024,
                 "h": 683,
                 "title": 'Building # 46 (E)'
             },
             {
-                "src": 'https://s3.eu-central-1.amazonaws.com/ar-fulda-app/E/e+(7).jpg',
+                "src": "assets/gallery/E/7.jpg",
                 "w": 1024,
                 "h": 683,
                 "title": 'Building # 46 (E)'
             },
             {
-                "src": 'https://s3.eu-central-1.amazonaws.com/ar-fulda-app/E/e+(8).jpg',
+                "src": "assets/gallery/E/8.jpg",
                 "w": 1024,
                 "h": 683,
                 "title": 'Building # 46 (E)'
             },
             {
-                "src": 'https://s3.eu-central-1.amazonaws.com/ar-fulda-app/E/e+(9).jpg',
+                "src": "assets/gallery/E/9.jpg",
                 "w": 1024,
                 "h": 683,
                 "title": 'Building # 46 (E)'
             },
             {
-                "src": 'https://s3.eu-central-1.amazonaws.com/ar-fulda-app/E/e+(10).jpg',
+                "src": "assets/gallery/E/10.jpg",
                 "w": 1024,
                 "h": 683,
                 "title": 'Building # 46 (E)'
             },
             {
-                "src": 'https://s3.eu-central-1.amazonaws.com/ar-fulda-app/E/e+(11).jpg',
+                "src": "assets/gallery/E/11.jpg",
                 "w": 1024,
                 "h": 683,
                 "title": 'Building # 46 (E)'
             },
             {
-                "src": 'https://s3.eu-central-1.amazonaws.com/ar-fulda-app/E/e+(12).jpg',
+                "src": "assets/gallery/E/12.jpg",
                 "w": 1024,
                 "h": 683,
                 "title": 'Building # 46 (E)'
             },
             {
-                "src": 'https://s3.eu-central-1.amazonaws.com/ar-fulda-app/E/e+(13).jpg',
+                "src": "assets/gallery/E/13.jpg",
                 "w": 1024,
                 "h": 683,
                 "title": 'Building # 46 (E)'
             },
             {
-                "src": 'https://s3.eu-central-1.amazonaws.com/ar-fulda-app/E/e+(14).jpg',
+                "src": "assets/gallery/E/14.jpg",
                 "w": 1024,
                 "h": 683,
                 "title": 'Building # 46 (E)'
@@ -188,40 +172,40 @@ var pointersData = [
         "name": "G (51)",
         "buildingName": "Building # 51 (G)",
         "description": "You can find offices of the Professors here (Prof. Todenhöfer, Prof. Herpers. etc). There are two computer labs with a printing room as well in this building.",
-        "thumbnailURL": "http://i.imgur.com/DnDoIjl.jpg",
+        "thumbnailURL": "assets/gallery/G/1.jpg",
         "buildingImages": [
             {
-                "src": "https://s3.eu-central-1.amazonaws.com/ar-fulda-app/G/G+(1).jpg",
+                "src": "assets/gallery/G/1.jpg",
                 "w": 964,
                 "h": 1024,
                 "title": 'Building # 51 (G)'
             },
             {
-                "src": "https://s3.eu-central-1.amazonaws.com/ar-fulda-app/G/G+(2).jpg",
+                "src": "assets/gallery/G/2.jpg",
                 "w": 964,
                 "h": 1024,
                 "title": 'Building # 51 (G)'
             },
             {
-                "src": "https://s3.eu-central-1.amazonaws.com/ar-fulda-app/G/G+(3).jpg",
+                "src": "assets/gallery/G/3.jpg",
                 "w": 964,
                 "h": 1024,
                 "title": 'Building # 51 (G)'
             },
             {
-                "src": "https://s3.eu-central-1.amazonaws.com/ar-fulda-app/G/G+(4).jpg",
+                "src": "assets/gallery/G/4.jpg",
                 "w": 964,
                 "h": 1024,
                 "title": 'Building # 51 (G)'
             },
             {
-                "src": "https://s3.eu-central-1.amazonaws.com/ar-fulda-app/G/G+(5).jpg",
+                "src": "assets/gallery/G/5.jpg",
                 "w": 964,
                 "h": 1024,
                 "title": 'Building # 51 (G)'
             },
             {
-                "src": "https://s3.eu-central-1.amazonaws.com/ar-fulda-app/G/G+(6).jpg",
+                "src": "assets/gallery/G/6.jpg",
                 "w": 964,
                 "h": 1024,
                 "title": 'Building # 51 (G)'
@@ -236,7 +220,7 @@ var pointersData = [
         "name": "Q (23)",
         "description": "Few of social sciences related classes held in this building. Beside this building you can find the offices of Studentenwerk.",
         "buildingName": "Building # 23 (Q)",
-        "thumbnailURL": "http://i.imgur.com/DnDoIjl.jpg",
+        "thumbnailURL": "https://s3.eu-central-1.amazonaws.com/ar-fulda-app/banners/hs_fulda.jpg"
         //"buildingImages": [
         //    {
         //        "src": "https://farm2.staticflickr.com/1043/5186867718_06b2e9e551_b.jpg",
@@ -254,46 +238,46 @@ var pointersData = [
         "name": "U (12)",
         "buildingName": "Building # 12 (U)",
         "description": "Bibliothek / Library of the university. Books can be borrowed from the library using library's web portal for 5 weeks. This can be extended as well.",
-        "thumbnailURL": "http://i.imgur.com/DnDoIjl.jpg",
+        "thumbnailURL": "assets/gallery/U/5.jpg",
         "buildingImages": [
             {
-                "src": "https://s3.eu-central-1.amazonaws.com/ar-fulda-app/U/U+(1).jpg",
+                "src": "assets/gallery/U/1.jpg",
                 "w": 964,
                 "h": 1024,
                 "title": 'Building # 12 (U)'
             },
             {
-                "src": "https://s3.eu-central-1.amazonaws.com/ar-fulda-app/U/U+(2).jpg",
+                "src": "assets/gallery/U/2.jpg",
                 "w": 964,
                 "h": 1024,
                 "title": 'Building # 12 (U)'
             },
             {
-                "src": "https://s3.eu-central-1.amazonaws.com/ar-fulda-app/U/U+(3).jpg",
+                "src": "assets/gallery/U/3.jpg",
                 "w": 964,
                 "h": 1024,
                 "title": 'Building # 12 (U)'
             },
             {
-                "src": "https://s3.eu-central-1.amazonaws.com/ar-fulda-app/U/U+(4).jpg",
+                "src": "assets/gallery/U/4.jpg",
                 "w": 964,
                 "h": 1024,
                 "title": 'Building # 12 (U)'
             },
             {
-                "src": "https://s3.eu-central-1.amazonaws.com/ar-fulda-app/U/U+(5).jpg",
+                "src": "assets/gallery/U/5.jpg",
                 "w": 964,
                 "h": 1024,
                 "title": 'Building # 12 (U)'
             },
             {
-                "src": "https://s3.eu-central-1.amazonaws.com/ar-fulda-app/U/U+(6).jpg",
+                "src": "assets/gallery/U/6.jpg",
                 "w": 964,
                 "h": 1024,
                 "title": 'Building # 12 (U)'
             },
             {
-                "src": "https://s3.eu-central-1.amazonaws.com/ar-fulda-app/U/U+(7).jpg",
+                "src": "assets/gallery/U/7.jpg",
                 "w": 964,
                 "h": 1024,
                 "title": 'Building # 12 (U)'
@@ -308,70 +292,70 @@ var pointersData = [
         "name": "T (11)",
         "description": "MENSA. This is basically the place to eat. Students gets subsidy on the lunch and refreshments. You need to charge your ID CARD using the machine outside for paying. No money accepts here.",
         "buildingName": "Building # 11 (T)",
-        "thumbnailURL": "http://i.imgur.com/DnDoIjl.jpg",
+        "thumbnailURL": "assets/gallery/T/9.jpg",
         "buildingImages": [
             {
-                "src": "https://s3.eu-central-1.amazonaws.com/ar-fulda-app/T/T+(1).jpeg",
+                "src": "assets/gallery/T/1.jpg",
                 "w": 964,
                 "h": 1024,
                 "title": 'Building # 11 (T)'
             },
             {
-                "src": "https://s3.eu-central-1.amazonaws.com/ar-fulda-app/T/T+(1).jpg",
+                "src": "assets/gallery/T/2.jpg",
                 "w": 964,
                 "h": 1024,
                 "title": 'Building # 11 (T)'
             },
             {
-                "src": "https://s3.eu-central-1.amazonaws.com/ar-fulda-app/T/T+(2).jpg",
+                "src": "assets/gallery/T/3.jpg",
                 "w": 964,
                 "h": 1024,
                 "title": 'Building # 11 (T)'
             },
             {
-                "src": "https://s3.eu-central-1.amazonaws.com/ar-fulda-app/T/T+(3).jpg",
+                "src": "assets/gallery/T/4.jpg",
                 "w": 964,
                 "h": 1024,
                 "title": 'Building # 11 (T)'
             },
             {
-                "src": "https://s3.eu-central-1.amazonaws.com/ar-fulda-app/T/T+(4).jpg",
+                "src": "assets/gallery/T/5.jpg",
                 "w": 964,
                 "h": 1024,
                 "title": 'Building # 11 (T)'
             },
             {
-                "src": "https://s3.eu-central-1.amazonaws.com/ar-fulda-app/T/T+(5).jpg",
+                "src": "assets/gallery/T/6.jpg",
                 "w": 964,
                 "h": 1024,
                 "title": 'Building # 11 (T)'
             },
             {
-                "src": "https://s3.eu-central-1.amazonaws.com/ar-fulda-app/T/T+(6).jpg",
+                "src": "assets/gallery/T/7.jpg",
                 "w": 964,
                 "h": 1024,
                 "title": 'Building # 11 (T)'
             },
             {
-                "src": "https://s3.eu-central-1.amazonaws.com/ar-fulda-app/T/T+(7).jpg",
+                "src": "assets/gallery/T/8.jpg",
                 "w": 964,
                 "h": 1024,
                 "title": 'Building # 11 (T)'
             },
             {
-                "src": "https://s3.eu-central-1.amazonaws.com/ar-fulda-app/T/T+(8).jpg",
+                "src": "assets/gallery/T/9.jpg",
                 "w": 964,
                 "h": 1024,
                 "title": 'Building # 11 (T)'
             },
             {
-                "src": "https://s3.eu-central-1.amazonaws.com/ar-fulda-app/T/T+(9).jpg",
+                "src": "assets/gallery/T/10.jpg",
                 "w": 964,
                 "h": 1024,
                 "title": 'Building # 11 (T)'
             },
             {
-                "src": "https://s3.eu-central-1.amazonaws.com/ar-fulda-app/T/T+(10).jpg",
+                "src": "assets/gallery/T/11.jpg",
                 "w": 964,
                 "h": 1024,
                 "title": 'Building # 11 (T)'
@@ -386,52 +370,52 @@ var pointersData = [
         "name": "S (10)",
         "buildingName": "Building # 10 (S)",
         "description": "SSC. International students service center where you can register yourself, get whatever information you want from front office. You can also visit to get advise from counsellors.",
-        "thumbnailURL": "http://i.imgur.com/DnDoIjl.jpg",
+        "thumbnailURL": "assets/gallery/S/4.jpg",
         "buildingImages": [
             {
-                "src": "	https://s3.eu-central-1.amazonaws.com/ar-fulda-app/S/s+(1).jpg",
+                "src": "assets/gallery/S/1.jpg",
                 "w": 964,
                 "h": 1024,
                 "title": 'Building # 10 (S)'
             },
             {
-                "src": "	https://s3.eu-central-1.amazonaws.com/ar-fulda-app/S/s+(2).jpg",
+                "src": "assets/gallery/S/2.jpg",
                 "w": 964,
                 "h": 1024,
                 "title": 'Building # 10 (S)'
             },
             {
-                "src": "	https://s3.eu-central-1.amazonaws.com/ar-fulda-app/S/s+(3).jpg",
+                "src": "assets/gallery/S/3.jpg",
                 "w": 964,
                 "h": 1024,
                 "title": 'Building # 10 (S)'
             },
             {
-                "src": "	https://s3.eu-central-1.amazonaws.com/ar-fulda-app/S/s+(4).jpg",
+                "src": "assets/gallery/S/4.jpg",
                 "w": 964,
                 "h": 1024,
                 "title": 'Building # 10 (S)'
             },
             {
-                "src": "	https://s3.eu-central-1.amazonaws.com/ar-fulda-app/S/s+(5).jpg",
+                "src": "assets/gallery/S/5.jpg",
                 "w": 964,
                 "h": 1024,
                 "title": 'Building # 10 (S)'
             },
             {
-                "src": "	https://s3.eu-central-1.amazonaws.com/ar-fulda-app/S/s+(6).jpg",
+                "src": "assets/gallery/S/6.jpg",
                 "w": 964,
                 "h": 1024,
                 "title": 'Building # 10 (S)'
             },
             {
-                "src": "	https://s3.eu-central-1.amazonaws.com/ar-fulda-app/S/s+(7).jpg",
+                "src": "assets/gallery/S/7.jpg",
                 "w": 964,
                 "h": 1024,
                 "title": 'Building # 10 (S)'
             },
             {
-                "src": "	https://s3.eu-central-1.amazonaws.com/ar-fulda-app/S/s+(8).jpg",
+                "src": "assets/gallery/S/8.jpg",
                 "w": 964,
                 "h": 1024,
                 "title": 'Building # 10 (S)'
@@ -446,15 +430,15 @@ var pointersData = [
         "name": "KL",
         "buildingName": "Kaufland Mart",
         "description": "Kaufland",
-        "thumbnailURL": "http://i.imgur.com/DnDoIjl.jpg"
+        "thumbnailURL": "assets/gallery/hs_fulda.jpg"
     },
     {
         "id": 11,
         "longitude": 9.66860443,
         "latitude": 50.54844334,
         "altitude": 247,
-        "name": "PRK",
+        "name": "PARK",
         "buildingName": "Jogging Park",
         "description": "Beautiful Park",
-        "thumbnailURL": "http://i.imgur.com/DnDoIjl.jpg"
+        "thumbnailURL": "assets/gallery/hs_fulda.jpg"
     }];
